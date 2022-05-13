6bc66cd097cda701e2c640d9827140c1e7e850c1
agus <agus.bazzi@gmail.com>
2022-05-12 22:44:21 -0300

    feat<Member> Member tiene lista de Journeys

--- src/main/java/domain/organization/Member.java
+++ src/main/java/domain/organization/Member.java
@@ -1,18 +1,25 @@
 package domain.organization;
 
 import domain.journey.Journey;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Member {
 
   private List<Sector> sectorList;
-  private Journey outwardJourney;
-  private Journey returnJourney;
+  private List<Journey> journeyList;
 
   private String name;
   private String lastName;
   private DocType docType;
   private String document;
+
+  public Member() {
+    this.sectorList = new ArrayList<>();
+    this.journeyList = new ArrayList<>();
+  }
+
 
 }
