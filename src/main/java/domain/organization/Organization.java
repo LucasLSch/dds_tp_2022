3d883e4535c770dd1681cc7a33cb326dfa7cbf16
Lucas Schnedier <llschneider2000@gmail.com>
2022-05-16 18:51:00 -0300

    feat <Organization>: Alta de organizaciones y sectores

--- src/main/java/domain/organization/Organization.java
+++ src/main/java/domain/organization/Organization.java
@@ -1,5 +1,6 @@
 package domain.organization;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Organization {
@@ -7,8 +8,22 @@ public class Organization {
   private List<Member> memberList;
   private List<Sector> sectorList;
   private String socialObjective;
-  private String ubication;
+  private String location;
   private String clasification;
   private OrgType orgType;
+
+  public Organization(String socObj, String locat, String clasific, OrgType orgType) {
+    this.socialObjective = socObj;
+    this.location = locat;
+    this.clasification = clasific;
+    this.orgType = orgType;
+    this.memberList = new ArrayList<>();
+    this.sectorList = new ArrayList<>();
+  }
+
+  public void singUpSector(Sector aSector) {
+    //this.validateSector(aSector) por si es necesario en un futuro
+    this.sectorList.add(aSector);
+  }
 
 }
