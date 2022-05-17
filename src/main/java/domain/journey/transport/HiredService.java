8a7fdf4a3ffdc40ae4d0808d8f97bf620d13450b
agus <agus.bazzi@gmail.com>
2022-05-17 20:55:53 -0300

    feat<HiredService>: Alta de HiredService

--- src/main/java/domain/journey/transport/HiredService.java
+++ src/main/java/domain/journey/transport/HiredService.java
@@ -3,4 +3,11 @@ package domain.journey.transport;
 public class HiredService implements Transport {
 
   private HiredServiceType hsType;
+  private String serviceName;
+
+  public HiredService(HiredServiceType someType, String someServiceName) {
+    this.hsType = someType;
+    this.serviceName = someServiceName;
+  }
+
 }
