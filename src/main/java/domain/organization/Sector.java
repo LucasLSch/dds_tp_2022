f11f4996f14af41dce4bd60de21f8a8756d43557
Lucas Schnedier <llschneider2000@gmail.com>
2022-06-06 20:44:46 -0300

    refactor <Sector>: constructor no longer tells Org to register a Sector

--- src/main/java/domain/organization/Sector.java
+++ src/main/java/domain/organization/Sector.java
@@ -8,7 +8,6 @@ public class Sector {
   public Sector(String sectorName, Organization organization) {
     this.sectorName = sectorName;
     this.organization = organization;
-    organization.registerSector(this);
   }
 
   public String getSectorName() {
