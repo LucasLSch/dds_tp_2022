b69cfd42a6736d3e4a51a6884a045fd822160302
Lucas Schnedier <llschneider2000@gmail.com>
2022-06-07 16:38:34 -0300

    feat <Location>: class Location

---
+++ src/main/java/domain/location/Location.java
@@ -0,0 +1,15 @@
+package domain.location;
+
+public class Location {
+  private Integer district;
+  private String street;
+  private String height;
+
+  public Location(Integer someDistrict, String someStreet, String someHeight) {
+    this.district = someDistrict;
+    this.street = someStreet;
+    this.height = someHeight;
+  }
+
+
+}
