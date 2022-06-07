a04bed67b6c5f55c686cda8c325b43b1564041da
Lucas Schnedier <llschneider2000@gmail.com>
2022-06-07 19:12:11 -0300

    feat <DistanceCalculatorAPIAdapter>: adapter class

--- src/main/java/domain/location/DistanceCalculatorAPIAdapter.java
+++ src/main/java/domain/location/DistanceCalculatorAPIAdapter.java
@@ -1,2 +1,22 @@
-package domain.location;public class DistanceCalculatorAPIAdapter {
+package domain.location;
+
+import API.SwaggerHubInterface;
+import retrofit2.Retrofit;
+
+public class DistanceCalculatorAPIAdapter {
+
+  private SwaggerHubInterface api;
+
+  public DistanceCalculatorAPIAdapter() {
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("https://ddstpa.com.ar")
+        .build();
+    this.api = retrofit.create(SwaggerHubInterface.class);
+  }
+
+  public Integer getDistance(Location origin, Location destiny) {
+    //TODO
+    return 100;
+  }
+
 }
