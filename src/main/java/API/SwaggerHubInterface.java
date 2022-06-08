76efc0e2adad1e781e7198500c96ead8f32cc09c
Lucas Schnedier <llschneider2000@gmail.com>
2022-06-08 16:16:45 -0300

    feat <SwaggerHubInterface>: API interface

--- src/main/java/API/SwaggerHubInterface.java
+++ src/main/java/API/SwaggerHubInterface.java
@@ -1,2 +1,15 @@
-package API;public interface SwaggerHubInterface {
+package API;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface SwaggerHubInterface {
+  @GET("api/distancia")
+  Call<Integer> distance(@Path("localidadOringenId") Integer originDistrct,
+                         @Path("calleOrigen") String originStreet,
+                         @Path("alturaOrigen") String originHeight,
+                         @Path("localidadDestinoId") Integer destinyDistrct,
+                         @Path("calleDestino") String destinyStreet,
+                         @Path("alturaDestino") String destinyHeight);
 }
