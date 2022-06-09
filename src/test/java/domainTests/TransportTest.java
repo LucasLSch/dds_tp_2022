b778091709440b16de6f46032aabe7771ad5a9ec
Lucas Schnedier <llschneider2000@gmail.com>
2022-06-09 10:58:34 -0300

    test <TransportTest>: distancia de distintos transportes

--- src/test/java/domainTests/TransportTest.java
+++ src/test/java/domainTests/TransportTest.java
@@ -1,2 +1,53 @@
-package domainTests;public class TransportTest {
+package domainTests;
+
+import domain.journey.Leg;
+import domain.journey.transport.*;
+import domain.location.Location;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import services.georef.Distance;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class TransportTest {
+  private Transport dummyPublic;
+  private Transport dummyHiredService;
+  private Line dummyLine;
+  private List<Stop> dummyStops = new ArrayList<>();
+  private Stop dummyStartStop;
+  private Stop dummyEndStop;
+  private Location mockedLocation;
+  private Location anotherMockedLocation;
+
+  @BeforeEach
+  public void beforeTest() {
+    mockedLocation = mock(Location.class);
+    anotherMockedLocation = mock(Location.class);
+    dummyStartStop = new Stop(mockedLocation, new Distance(10, "KM"), 0);
+    dummyEndStop = new Stop(anotherMockedLocation, new Distance(0, "KM"), 1);
+    dummyStops.add(dummyStartStop);
+    dummyStops.add(dummyEndStop);
+    dummyLine = new Line(dummyStops, "Cool line", PublicTransportType.BUS);
+    dummyPublic = new PublicTransport(dummyLine, dummyStartStop, dummyEndStop);
+    dummyHiredService = new HiredService(HiredServiceType.TAXI, "Radio Taxi");
+  }
+
+  @Test
+  public void publicTransportDistance() throws IOException {
+    Distance distance = dummyPublic.getDistance(mockedLocation, anotherMockedLocation);
+    assertEquals(distance.getValue(), 10);
+  }
+
+  @Test
+  public void hiredServiceTransportDistance() throws IOException {
+    when(mockedLocation.getDistanceTo(anotherMockedLocation)).thenReturn(new Distance(20, "KM"));
+
+    Distance distance = dummyHiredService.getDistance(mockedLocation, anotherMockedLocation);
+    assertEquals(distance.getValue(), 20);
+  }
 }
