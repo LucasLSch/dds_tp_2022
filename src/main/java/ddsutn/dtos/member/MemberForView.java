3bef1bcbf26060cb86021289529a83dcd4364a18
LucasLSch <llschneider2000@gmail.com>
2022-12-02 01:51:34 -0300

    feat<memberDetails>: view del miembro

--- src/main/java/ddsutn/dtos/member/MemberForView.java
+++ src/main/java/ddsutn/dtos/member/MemberForView.java
@@ -1,5 +1,6 @@
 package ddsutn.dtos.member;
 
+import ddsutn.domain.contact.Contact;
 import ddsutn.domain.organization.Member;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +17,15 @@ public class MemberForView {
   public String email;
   public String docType;
   public String documentNumber;
+  public String phoneNumber;
   public List<String> sectors;
 
 
   public MemberForView(Member someMember) {
     this.name = someMember.getName();
     this.surname = someMember.getSurname();
-    //this.email = someMember.getContacts().stream().findFirst().get().getEmail();
+    this.email = someMember.getContacts().stream().map(Contact::getEmail).findFirst().orElse("");
+    this.phoneNumber = someMember.getContacts().stream().map(Contact::getPhoneNumber).findFirst().orElse("");
     this.docType = someMember.getDocType().toString();
     this.documentNumber = someMember.getDocNumber();
     this.sectors = someMember.getSectors().stream().map(Objects::toString).collect(Collectors.toList());
